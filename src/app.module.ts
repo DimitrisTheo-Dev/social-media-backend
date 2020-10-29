92a8c5de908ad00061c8e7c61105071e61c2d5d2
DimitrisTheodoropoulos <dimitriss.theodoropoulos@gmail.com>
2020-10-29 20:23:46 +0200

    Added user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConnectionService } from './database-connection.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: DatabaseConnectionService,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
